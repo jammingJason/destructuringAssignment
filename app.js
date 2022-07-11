9d202738d165b8e85b08e204b6a6680ceb017352
Van Jason Crews <vanjasoncrews@gmail.com>
2022-07-11 08:59:27 -0400

    completed js file

--- app.js
+++ app.js
@@ -1,48 +1,50 @@
-// const obj = {
-// 	numbers: {
-// 		a: 1,
-// 		b: 2
-// 	}
+let { numbers: { a, b } } = obj;
+
+const arr = [ 1, 2 ];
+[ ([ arr[0], arr[1] ] = [ arr[1], arr[0] ]) ];
+
+const raceResults = ([ ...drivers ]) => (
+	([ first, second, third, ...rest ] = drivers), { first, second, third, rest: rest }
+);
+
+// let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+// let {numPlanets, yearNeptuneDiscovered} = facts;
+// console.log(numPlanets); // 8
+// console.log(yearNeptuneDiscovered); // 1846
+
+// let planetFacts = {
+//   numPlanets: 8,
+//   yearNeptuneDiscovered: 1846,
+//   yearMarsDiscovered: 1659
 // };
+// let {numPlanets, ...discoveryYears} = planetFacts;
+// console.log(discoveryYears); // {1846, 1659}
 
-// let { numbers: { a, b } } = obj;
-// var a = obj.numbers.a;
-// var b = obj.numbers.b;
+// function getUserData({firstName, favoriteColor="green"}){
+//   return `Your name is ${firstName} and you like ${favoriteColor}`;
+// }
+// getUserData({firstName: "Alejandro", favoriteColor: "purple"}) //
+// Your name is Alejandro and you like purple
+// getUserData({firstName: "Melissa"}) // Your name is Melissa and you like green
+// getUserData({}) // your name is undefined and you like green
 
-// const arr = [ 1, 2 ];
-// [ ([ arr[0], arr[1] ] = [ arr[1], arr[0] ]) ];
-// const temp = arr[0];
-// arr[0] = arr[1];
-// arr[1] = temp;
+// let [first, second, third] = ["Maya", "Marisa", "Chi"];
+// console.log(first); // Maya
+// console.log(second); // Marisa
+// console.log(third); // Chi
 
-const raceResults = ([ ...drivers ]) => {
-	let newObj = ({ first, second, third, ...theRest } = drivers);
-	// const newObj = {
-	// 	first,
-	// 	second,
-	// 	third
-	// };
+// let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+//   "Raindrops on roses",
+//   "whiskers on kittens",
+//   "Bright copper kettles",
+//   "warm woolen mittens",
+//   "Brown paper packages tied up with strings"
+// ]
+// console.log(raindrops); // Raindrops on roses
+// console.log(whiskers); // whiskers on kittens
+// console.log(aFewOfMyFavoriteThings); // [‘Bright copper kettles’, ‘warm woolen mittens’, ‘Brown paper packages tied up with strings’]
 
-	console.log(newObj);
-};
-// Write a function called raceResults which accepts a single array argument.
-// It should return an object with the keys first, second, third, and rest.
+// let numbers = [10, 20, 30];
+// [numbers[1], numbers[2]] = [numbers[2], numbers[1]]
 
-// first: the first element in the array
-// second: the second element in the array
-// third: the third element in the array
-// rest: all other elements in the array
-// Write a one line function to make this work using
-// An arrow function
-// Destructuring
-// ‘Enhanced’ object assignment (same key/value shortcut)
-raceResults([ 'Tom', 'Margaret', 'Allison', 'David', 'Pierre' ]);
-
-/*
-  {
-    first: "Tom",
-    second: "Margaret",
-    third: "Allison",
-    rest: ["David", "Pierre"]
-  }
-*/
+// console.log(numbers) // [10, 30, 20]
